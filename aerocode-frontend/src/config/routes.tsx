9d6ed6b861ba0914c818b903a9d24481100bf230
Laís Zanardi Inocêncio <89917698+lais-zanardi@users.noreply.github.com>
2025-11-02 15:49:12 -0300

    feat: define rota não protegida como Login,  rota protegida (MainLayout como componente pai) e rotas filhas renderizadas dentro do Outlet de MainLayout

--- aerocode-frontend/src/config/routes.tsx
+++ aerocode-frontend/src/config/routes.tsx
@@ -1,25 +1,29 @@
-import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import MainLayout from '../components/layout/MainLayout';
-import Login from '../pages/Login'; 
-import UserManagement from '../pages/UserManagement';
-import { useAuth } from '../context/AuthContext';
-import Home from '../pages/Home';
+import React from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import MainLayout from '../components/layout/MainLayout'
+import Login from '../pages/Login' 
+import UserManagement from '../pages/UserManagement'
+import { useAuth } from '../context/AuthContext'
+import Home from '../pages/Home'
 
 const AppRoutes: React.FC = () => {
-    const { isAuthenticated } = useAuth()
+    const { isAuthenticated } = useAuth() 
+
     return (
         <Routes>
             <Route path="/login" element={<Login />} />
-            <Route path="/" element={isAuthenticated ? <MainLayout /> : <Navigate to="/login" />}/>
-            <Route index element={<Home/>}/>
-            <Route path="admin/users" element={<UserManagement />} />
-            
-            
-            {/* Catch-all para rotas não encontradas */}
-            <Route path="*" element={<Navigate to="/" replace />} />
+            <Route 
+                path="/" 
+                element={isAuthenticated ? <MainLayout /> : <Navigate to="/login" />} 
+            >
+                <Route index element={<Home />} /> 
+                <Route path="admin/users" element={<UserManagement />} />
+                <Route path="aeronaves" element={<p>Página de Listagem de Aeronaves</p>} />
+                <Route path="*" element={<Navigate to="/" replace />} />
+                
+            </Route>
         </Routes>
-    );
-};
+    )
+}
 
-export default AppRoutes;
+export default AppRoutes
